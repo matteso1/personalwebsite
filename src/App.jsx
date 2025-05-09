be0733099d66d6eb95b0f18ecb87719a8a53b895
Nils Matteson <nilsmatteson@icloud.com>
2025-05-09 13:08:31 -0500

    chore: add scaffolded site components & fix PostCSS config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Music from './pages/Music';
